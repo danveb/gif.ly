ea8870c17734697634c1aed7cae9181c56fe0090
Daniel Bae <danibae91@icloud.com>
2022-11-03 08:58:38 -0700

    simple GIF fetcher with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react"; 
+import { Grid } from "@giphy/react-components";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import "./App.css"; 
+
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState(""); 
+
+    const gf = new GiphyFetch(process.env.REACT_APP_GIPHY_API_KEY); 
+    const fetchGifs = (offset) => gf.search(searchTerm, { offset, limit: 15 });
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value); 
+    };
+
     return (
-        <p>Hello World</p>
+        <div className="app">
+            <h5>Random GIF Grid</h5>
+            <form>
+                <label htmlFor="search">Enter Search Term</label>
+                <input 
+                    id="search"
+                    type="text"
+                    name="search"
+                    value={searchTerm}
+                    placeholder="Enter something..."
+                    onChange={handleChange}
+                />
+            </form>
+            <Grid width={800} columns={3} fetchGifs={fetchGifs} key={searchTerm} />
+        </div>
     )
 }
 
